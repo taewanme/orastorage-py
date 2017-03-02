33b383ef9858fe16503451de781fcc6553592655
Taewan <taewanme@gmail.com>
2017-03-02 15:50:42 +0900

    modify test logig for 2.6 compability.

--- tests/test_authenticate_a_user.py
+++ tests/test_authenticate_a_user.py
@@ -22,32 +22,31 @@ class TestAuthenticate_a_user(TestCase):
         invalid_identity = Identity(identity.get_identity_domain(),
                                     identity.get_user_id(),
                                     identity.get_password()+'1')
-        with self.assertRaises(REST401Exception) as context:
-            authenticate_a_user(invalid_identity.get_user_id(),
-                                invalid_identity.get_password(),
-                                invalid_identity.get_identity_domain())
+        self.assertRaises(REST401Exception, authenticate_a_user,
+                          invalid_identity.get_user_id(),
+                          invalid_identity.get_password(),
+                          invalid_identity.get_identity_domain())
 
     def test_test_authenticate_a_user_with_invalid_id(self):
         identity = get_identity()
         invalid_identity = Identity(identity.get_identity_domain(),
                                     identity.get_user_id()+'1',
                                     identity.get_password())
-        with self.assertRaises(REST401Exception) as context:
-            authenticate_a_user(invalid_identity.get_user_id(),
-                                invalid_identity.get_password(),
-                                invalid_identity.get_identity_domain())
+        self.assertRaises(REST401Exception, authenticate_a_user,
+                          invalid_identity.get_user_id(),
+                          invalid_identity.get_password(),
+                          invalid_identity.get_identity_domain())
 
     def test_test_authenticate_a_user_with_invalid_domain(self):
         identity = get_identity()
         invalid_identity = Identity(identity.get_identity_domain()+'1',
                                     identity.get_user_id(),
                                     identity.get_password())
-        with self.assertRaises(ConnectionError) as context:
-            authenticate_a_user(invalid_identity.get_user_id(),
-                                invalid_identity.get_password(),
-                                invalid_identity.get_identity_domain())
+        self.assertRaises(ConnectionError, authenticate_a_user,
+                               invalid_identity.get_user_id(),
+                               invalid_identity.get_password(),
+                               invalid_identity.get_identity_domain())
 
-        print(context.exception)
-        print(context.exception.strerror)
+
 
 
